90f235dae1bafdcb4126811a6b425a128fbb6bc0
Robertas Petrauskas <robertas.petrauskas0@gmail.com>
2021-01-16 14:40:59 +0200

    .

--- test/services/userService.test.js
+++ test/services/userService.test.js
@@ -1,2 +1,41 @@
 const assert = require('chai').assert;
-const userService = require('../../services/userService');
+const User = require('../../models/User');
+const sinon = require('sinon');
+const UserService = require('../../services/userService');
+const userService = new UserService(User);
+
+let userObj = {
+    "_id": "5ff9ca813844e025945cf8ab",
+    "name": "HitchHike-Admin",
+    "email": "admin@hitchhike.com",
+    "password": "password",
+    "phoneNumber": "+37065376764",
+    "country": "Lietuva",
+    "city": "Kaunas",
+    "address": "Ukmergės g. 22",
+    "postalCode": "49315",
+    "role": "ADMIN"
+}
+
+let userObj2 = {
+    "_id": "5ff9ca813844e025945cf8ab",
+    "name": "HitchHike-Admin",
+    "email": "admin@hitchhike.com",
+    "password": "password2",
+    "phoneNumber": "+37065376764",
+    "country": "Lietuva",
+    "city": "Kaunas",
+    "address": "Ukmergės g. 22",
+    "postalCode": "49315",
+    "role": "ADMIN"
+}
+const getByIdStub = sinon.stub(User, 'getUserById').returns(userObj)
+const updateStub = sinon.stub(User, 'updateUser').returns(userObj)
+
+describe('userService', () =>{
+    it('update', () =>{
+
+    
+
+    })
+})
